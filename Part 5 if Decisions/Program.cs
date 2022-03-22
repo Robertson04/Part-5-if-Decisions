a3a9801807828f36b655ec316499ff84992b3168
Cameron Robertson <RobeCame259@GoTVDSB.ca>
2022-03-22 11:03:47 -0400

    Add project files.

---
+++ Part 5 if Decisions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Part_5_if_Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stages
+            Console.WriteLine("Please enter a age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+                Console.WriteLine("Adult");
+
+            else if (age <= 18 && age > 12)
+                Console.WriteLine("Teen");
+
+            else if (age <= 12 && age > 10)
+                Console.WriteLine("Preteen");
+
+            else if (age <= 10 && age > 5)
+                Console.WriteLine("Child");
+
+            else if (age <= 5)
+                Console.WriteLine("Toddler");
+        }
+    }
+}
